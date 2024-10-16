b66de20c45603a23f6edad01ec14660d5a46f2a9
emesonmikael <emesonmikael@gmail.com>
2024-10-16 20:10:17 -0300

    r

--- server.js
+++ server.js
@@ -34,20 +34,9 @@ app.use('/proxy/:streamName', (req, res, next) => {
 
 
 // Rota para servir a playlist M3U8 com URLs proxy
-app.get('/playlist.m3u8', (req, res) => {
+app.get('/http://cdn-br.in/movie/978460358/473005646/1227625.mp4', (req, res) => {
   const playlist = `
-#EXTM3U
-#EXT-X-SESSION-DATA:DATA-ID="com.xui.1_5_13"
-#EXTINF:-1 tvg-id="a.e.br" tvg-name="A&E FHD" tvg-logo="https://i.ibb.co/DzvKbBK/AEdw-DAdbs.png" group-title="Canais || Filmes & Séries",A&E FHD
-https://proxy-server-3nlb.onrender.com/proxy/1227625.mp4
-#EXTINF:-1 tvg-id="a.e.br" tvg-name="A&E FHD H.265" tvg-logo="https://i.ibb.co/DzvKbBK/AEdw-DAdbs.png" group-title="Canais || Filmes & Séries",A&E FHD H.265
-https://m3u8-proxy.onrender.com/proxy/628829.m3u8
-#EXTINF:-1 tvg-id="a.e.br" tvg-name="A&E HD" tvg-logo="https://i.ibb.co/DzvKbBK/AEdw-DAdbs.png" group-title="Canais || Filmes & Séries",A&E HD
-https://m3u8-proxy.onrender.com/proxy/628398.m3u8
-#EXTINF:-1 tvg-id="a.e.br" tvg-name="A&E SD" tvg-logo="https://i.ibb.co/DzvKbBK/AEdw-DAdbs.png" group-title="Canais || Filmes & Séries",A&E SD
-https://m3u8-proxy.onrender.com/proxy/628399.m3u8
-#EXTINF:-1 tvg-id="" tvg-name="AE Alterosa Esporte" tvg-logo="https://i.ibb.co/kmDVR7H/52640e104071781-5f5a9c3f82521.png" group-title="Canais || Pay Per View",AE Alterosa Esporte
-https://m3u8-proxy.onrender.com/proxy/628461.m3u8
+
   `;
   res.set('Content-Type', 'application/vnd.apple.mpegurl');
   res.send(playlist);
